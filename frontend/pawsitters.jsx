7d4bbc114ad3bfd525f13cd2ab80bfdfde1da473
charles mancuso <charlesmancuso66@yahoo.com>
2019-06-25 10:35:16 -0700

    Finished reducers for errors and user auth. made a root reducer and store and checked window getstate for accuracy

--- frontend/pawsitters.jsx
+++ frontend/pawsitters.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { signup, login, logout } from './util/session_api_util';
+import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root');
     ReactDOM.render(<h1>Welcome to Pawsitters!!!</h1>, root);
+    const store = configureStore();
     window.signup = signup;
     window.login = login;
     window.logout = logout;
+
+    window.getState = store.getState;
+    window.dispatch = store.dispatch; // just for testing!
 });
