b9fee3eedcf049c73abd057c2be86faabcc73943
charles mancuso <charlesmancuso66@yahoo.com>
2019-06-26 10:58:45 -0700

    fill in user seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.delete_all
+
+User.create!(username: "charles", password: "charles", email: "charles@yahoo.com")
+User.create!(username: "jennifer", password: "jennifer", email: "jennifer@yahoo.com")
+User.create!(username: "marco", password: "marco", email: "marco@yahoo.com")
+User.create!(username: "tanya", password: "tanya", email: "tanya@yahoo.com")
+User.create!(username: "trevor", password: "trevor", email: "trevor@yahoo.com")
